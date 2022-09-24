36e53acf805073ab4db472b338bbc7c82d942c4f
HZ <335672336@qq.com>
2022-09-25 01:02:31 +0800

    initial commit

---
+++ src/view/main.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div>Main</div>
+    <!-- <button @click="increment">setup state: {{state.count}}</button> -->
+    <!-- <button @click="incDataCount">data count: {{count}}</button> -->
+    <button @click="changeVal1">change val1</button>
+    <button @click="changeVal2">change val2</button>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+   data() {
+     return {
+        count: 0
+     }
+   },
+   methods: {
+    incDataCount() {
+        console.log('this.count',this.count);
+        console.log('state',this.state);
+        this.count ++
+    }
+   }
+}
+</script>
+
+<script setup>
+  import {ref, reactive, watch, watchEffect } from 'vue'
+
+const state = reactive({ count: 0 })
+
+function increment() {
+  state.count++
+}
+
+let val1 = ref(0)
+let val2 = ref(100)
+
+watchEffect(() => {
+  val1.value++
+  val2.value--
+  console.log('val1',val1.value);
+  console.log('val2',val2.value);
+})
+
+function changeVal1() {
+  val1.value++
+}
+function changeVal2() {
+  val2.value--
+}
+
+</script>
+
+
+<style>
+
+</style>
---
+++ src/view/test.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+     <div v-if="!!obj.bar">bar存在</div>
+     <div v-if="!obj.bar">bar不存在</div>
+     <button @click="setBar">set bar</button>
+     <button @click="deleteBar">delete bar</button>
+  </div>
+</template>
+
+<script>
+ 
+export default {
+  name: 'App',
+  data() {
+    return {
+      obj: {
+        'foo': 'foo'
+      }
+    }
+  },
+  methods: { 
+    setBar() {
+      this.obj.bar = 'bar'
+      console.log('set bar complete',!!this.obj.bar);
+    },
+    deleteBar() {
+      delete this.obj.bar
+      console.log('delete bar complete',!!this.obj.bar);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
